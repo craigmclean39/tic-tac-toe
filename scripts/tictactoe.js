b67a07ae04d099a6712c6b5e9ef382d3e7fbac74
Craig McLean <craig.mclean39@gmail.com>
2021-07-24 11:18:43 -0700

    Minimax function created and it seems to work. Still need to find the best move for the computer and then make it.

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -4,100 +4,164 @@ var gameState = (function() {
     let _gameBoard = [];
     let _playerChar = 'x';
     let _cpuChar = 'o';
+    let _emptyChar = "_";
 
-    function _evaluateBoard()
+    function _isBoardInTerminalState(gameBoard)
+    {
+        for(let i = 0; i < gameBoard.length; i++)
+        {
+            for(let j = 0; j < gameBoard[i].length; j++)
+            {
+                if(gameBoard[i][j] == _emptyChar)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    function _minimax(gameBoard, isCpu)
+    {
+        if(_isBoardInTerminalState(gameBoard))
+        {
+            return _evaluateBoard(gameBoard);
+        }
+
+        if(isCpu)
+        {
+            let bestValue = -Infinity;
+            //As the cpu, look for an empty spot and make that move
+            for(let i = 0; i < gameBoard.length; i++)
+            {
+                for(let j = 0; j < gameBoard[i].length; j++)
+                {
+                    if(gameBoard[i][j] == _emptyChar)
+                    {
+                        gameBoard[i][j] = _cpuChar;
+                        
+                        //now recursively go through the function again, but as the players turn
+                        let value = _minimax(gameBoard, false);
+                        bestValue = Math.max(bestValue, value);
+                    }
+                }
+            }
+            return bestValue;
+        }
+        else
+        {
+            let bestValue = +Infinity;
+            for(let i = 0; i < gameBoard.length; i++)
+            {
+                for(let j = 0; j < gameBoard[i].length; j++)
+                {
+                    if(gameBoard[i][j] == _emptyChar)
+                    {
+                        gameBoard[i][j] = _playerChar;
+                        let value = _minimax(gameBoard, true);
+                        bestValue = Math.min(bestValue, value);
+                    }
+                }
+            }
+            return bestValue;
+        }
+        
+    }
+
+    function _evaluateBoard(gameBoard)
     {
         //check columns for win condition
-        if(_gameBoard[0][0] == _gameBoard[0][1] && _gameBoard[0][0] == _gameBoard[0][2])
+        if(gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][0] == gameBoard[0][2])
         {
-            if(_gameBoard[0][0] == _playerChar)
+            if(gameBoard[0][0] == _playerChar)
             {
                 //player wins
                 return -10;
             }
-            else if(_gameBoard[0][0] == _cpuChar)
+            else if(gameBoard[0][0] == _cpuChar)
             {
                 //cpu wins
                 return 10;
             }
         }
-        else if(_gameBoard[1][0] == _gameBoard[1][1] && _gameBoard[1][0] == _gameBoard[1][2])
+        else if(gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][0] == gameBoard[1][2])
         {
-            if(_gameBoard[1][0] == _playerChar)
+            if(gameBoard[1][0] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[1][0] == _cpuChar)
+            else if(gameBoard[1][0] == _cpuChar)
             {
                 return 10;
             }
         }
-        else if(_gameBoard[2][0] == _gameBoard[2][1] && _gameBoard[2][0] == _gameBoard[2][2])
+        else if(gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][0] == gameBoard[2][2])
         {
-            if(_gameBoard[2][0] == _playerChar)
+            if(gameBoard[2][0] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[2][0] == _cpuChar)
+            else if(gameBoard[2][0] == _cpuChar)
             {
                 return 10;
             }
         }
 
         //check rows for win condition
-        if(_gameBoard[0][0] == _gameBoard[1][0] && _gameBoard[0][0] == _gameBoard[2][0])
+        if(gameBoard[0][0] == gameBoard[1][0] && gameBoard[0][0] == gameBoard[2][0])
         {
-            if(_gameBoard[0][0] == _playerChar)
+            if(gameBoard[0][0] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[0][0] == _cpuChar)
+            else if(gameBoard[0][0] == _cpuChar)
             {
                 return 10;
             }
         }
-        else if(_gameBoard[0][1] == _gameBoard[1][1] && _gameBoard[0][1] == _gameBoard[2][1])
+        else if(gameBoard[0][1] == gameBoard[1][1] && gameBoard[0][1] == gameBoard[2][1])
         {
-            if(_gameBoard[0][1] == _playerChar)
+            if(gameBoard[0][1] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[0][1] == _cpuChar)
+            else if(gameBoard[0][1] == _cpuChar)
             {
                 return 10;
             }
         }
-        else if(_gameBoard[0][2] == _gameBoard[1][2] && _gameBoard[0][2] == _gameBoard[2][2])
+        else if(gameBoard[0][2] == gameBoard[1][2] && gameBoard[0][2] == gameBoard[2][2])
         {
-            if(_gameBoard[0][2] == _playerChar)
+            if(gameBoard[0][2] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[0][2] == _cpuChar)
+            else if(gameBoard[0][2] == _cpuChar)
             {
                 return 10;
             }
         }
 
         //check diagonals for win condition
-        if(_gameBoard[0][0] == _gameBoard[1][1] && _gameBoard[0][0] == _gameBoard[2][2])
+        if(gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2])
         {
-            if(_gameBoard[0][0] == _playerChar)
+            if(gameBoard[0][0] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[0][0] == _cpuChar)
+            else if(gameBoard[0][0] == _cpuChar)
             {
                 return 10;
             }
         }
-        else if(_gameBoard[0][2] == _gameBoard[1][1] && _gameBoard[0][2] == _gameBoard[2][0])
+        else if(gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0])
         {
-            if(_gameBoard[0][2] == _playerChar)
+            if(gameBoard[0][2] == _playerChar)
             {
                 return -10;
             }
-            else if(_gameBoard[0][2] == _cpuChar)
+            else if(gameBoard[0][2] == _cpuChar)
             {
                 return 10;
             }
@@ -136,7 +200,9 @@ var gameState = (function() {
         _gameBoard[usrSelection[0]][usrSelection[1]] = "x";
         printBoard();
 
-        console.log(`Evaluate: ${_evaluateBoard()}`);
+        //console.log(`Evaluate: ${_evaluateBoard()}`);
+
+        console.log(_minimax(_gameBoard, true));
 
     }
 
