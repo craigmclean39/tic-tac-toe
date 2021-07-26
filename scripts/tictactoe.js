a0c66ccff8ad2cda61741406834a8c451cdbee0b
Craig McLean <craig.mclean39@gmail.com>
2021-07-26 10:46:05 -0700

    new minimax and findbestmove functions. It seems to choose the proper square now.

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -28,18 +28,6 @@ var gameManager = (function() {
         return true;
     }
 
-    function _minimax(gBoard, isMaximizer)
-    {
-
-       
-        
-    }
-
-    function _findBestMove(gBoard)
-    {
-
-    }
-
     function _evaluateBoard(gBoard)
     {
         let winState = null;
@@ -100,6 +88,110 @@ var gameManager = (function() {
             return winState;
         }
     }
+
+    function _minimax(gBoard, isMaximizer)
+    {
+        let boardStatus = _evaluateBoard(gBoard);
+
+        //if there is a tie, or either player is the winner, return
+        if(boardStatus != null)
+        {
+            //console.log("winstate");
+            return _winStates[boardStatus];
+        }
+
+
+        if(isMaximizer)
+        {
+            //Start with a low possible score
+            let bestScore = -1000;
+
+            //Go through every cell, if it's empty, put the cpu there and recursively call this function
+
+            for(let i = 0; i < 3; i++)
+            {
+                for(let j = 0; j < 3; j++)
+                {
+                    if(gBoard[i][j] == _emptyChar)
+                    {
+                        //do the cpu move
+                        gBoard[i][j] = _cpuChar;
+
+                        let score = _minimax(gBoard, false);
+
+                        //undo the cpu move
+                        gBoard[i][j] = _emptyChar;
+
+                        bestScore = Math.max(score, bestScore);
+
+                    }
+                }
+            }
+            //console.log(bestScore);
+            return bestScore;
+        }
+        else
+        {
+            //Start with a high possible score
+            let bestScore = 1000;
+
+            //Go through every cell, if it's empty, put the cpu there and recursively call this function
+
+            for(let i = 0; i < 3; i++)
+            {
+                for(let j = 0; j < 3; j++)
+                {
+                    if(gBoard[i][j] == _emptyChar)
+                    {
+                        //do the player(minimizer) move
+                        gBoard[i][j] = _playerChar;
+
+                        let score = _minimax(gBoard, true);
+
+                        //undo the cpu move
+                        gBoard[i][j] = _emptyChar;
+
+                        bestScore = Math.min(score, bestScore);
+                    }
+                }
+            }
+            //console.log(bestScore);
+            return bestScore;
+        }
+    }
+
+    function _findBestMove(gBoard)
+    {
+        let bestScore = -1000;
+        let bestRow = -1;
+        let bestCol = -1;
+
+        for(let i = 0; i < 3; i++)
+        {
+            for(let j = 0; j < 3; j++)
+            {
+                if(gBoard[i][j] == _emptyChar)
+                {
+                    gBoard[i][j] = _cpuChar;
+
+                    let moveScore = _minimax(gBoard, false);
+
+                    gBoard[i][j] = _emptyChar;
+
+                    if(moveScore > bestScore)
+                    {
+                        bestScore = moveScore;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+
+                }
+            }
+        }
+
+        console.log(`Best move value is ${bestScore} at row ${bestRow} and col ${bestCol}`)
+
+    }
   
     function printBoard(gBoard) {
       for(let i = 0; i < 3; i++)
@@ -123,7 +215,7 @@ var gameManager = (function() {
 
     function testEval() {
 
-        let testBoardRowPlayer = [];
+        /* let testBoardRowPlayer = [];
         let testBoardRowCpu = [];
         let testBoardColPlayer = [];
         let testBoardColCpu = [];
@@ -180,7 +272,15 @@ var gameManager = (function() {
         printBoard(testBoardTie0);
         console.log(_evaluateBoard(testBoardTie0));
         printBoard(testBoardUnfinishedGame);
-        console.log(_evaluateBoard(testBoardUnfinishedGame));
+        console.log(_evaluateBoard(testBoardUnfinishedGame)); */
+
+
+        let testMini = [];
+        testMini.push([_emptyChar,_emptyChar,_emptyChar]);
+        testMini.push([_emptyChar,_emptyChar,_emptyChar]);
+        testMini.push([_emptyChar,_emptyChar,_playerChar]);
+
+        _findBestMove(testMini);
 
     }
 
